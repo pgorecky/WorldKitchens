56b50e70229476125edf32d2bb1a3169c1d4fa9a
Patryk <patryk.gorecky@gmail.com>
2023-10-26 19:15:34 +0200

    init commit

---
+++ WorldsKitchenServer/src/main/java/com/sigma/worldskitchenserver/WorldsKitchenServerApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.worldskitchenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsKitchenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldsKitchenServerApplication.class, args);
+    }
+
+}
